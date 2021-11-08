cd17a03f53547102c8b6b39cc865407981a8548b
Clint Edwards <cedwards1080@gmail.com>
2021-11-08 13:30:29 -0700

    Had the url paths backwards

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,14 +7,14 @@ import Link from "../components/Navigation";
 function App() {
   return (
     <div className="App">
-      <Route path="/">
-        <Link href="/contact" className="contact_header_background">
+      <Route path="/contact">
+        <Link href="/" className="contact_header_background">
           home
         </Link>
         <Contact />
       </Route>
-      <Route path="/contact">
-        <Link href="/" className="contact">
+      <Route path="/">
+        <Link href="/contact" className="contact">
           contact
         </Link>
         <Home />
